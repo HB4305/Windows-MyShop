e352d863e5074b181e9ae68a3ab1c101e9627878
LamKhanhDepzai <lamhuukhanh69@gmail.com>
2026-04-05 18:53:01 +0700

    fix login page toggle password

--- MyShop/Views/LoginPage.xaml.cs
+++ MyShop/Views/LoginPage.xaml.cs
@@ -16,16 +16,12 @@ public sealed partial class LoginPage : Page
 
         Loaded += OnLoaded;
         PasswordBox.PasswordChanged += OnPasswordChanged;
-        PasswordTextBox.TextChanged += OnPasswordTextBoxTextChanged;
     }
 
     private void OnLoaded(object sender, RoutedEventArgs e)
     {
         if (DataContext is LoginViewModel vm)
-        {
             PasswordBox.Password = vm.Password;
-            PasswordTextBox.Text = vm.Password;
-        }
     }
 
     private void OnPasswordChanged(object sender, RoutedEventArgs e)
@@ -34,33 +30,10 @@ public sealed partial class LoginPage : Page
             vm.Password = PasswordBox.Password;
     }
 
-    private void OnPasswordTextBoxTextChanged(object sender, TextChangedEventArgs e)
+    private void TogglePassword_Click(object sender, RoutedEventArgs e)
     {
-        if (DataContext is LoginViewModel vm)
-            vm.Password = PasswordTextBox.Text;
-    }
-
-    private bool _isPasswordVisible = false;
-
-    private void OnTogglePasswordClick(object sender, RoutedEventArgs e)
-    {
-        if (_isPasswordVisible)
-        {
-            PasswordBox.Password = PasswordTextBox.Text;
-            PasswordBox.Visibility = Visibility.Visible;
-            PasswordTextBox.Visibility = Visibility.Collapsed;
-            TogglePasswordIcon.Text = "\uE7B3";
-            ToolTipService.SetToolTip(TogglePasswordButton, "Show password");
-            _isPasswordVisible = false;
-        }
-        else
-        {
-            PasswordTextBox.Text = PasswordBox.Password;
-            PasswordBox.Visibility = Visibility.Collapsed;
-            PasswordTextBox.Visibility = Visibility.Visible;
-            TogglePasswordIcon.Text = "\uE7B4";
-            ToolTipService.SetToolTip(TogglePasswordButton, "Hide password");
-            _isPasswordVisible = true;
-        }
+        PasswordBox.PasswordRevealMode = PasswordBox.PasswordRevealMode == PasswordRevealMode.Visible
+            ? PasswordRevealMode.Hidden
+            : PasswordRevealMode.Visible;
     }
 }
